bf20371719fe6868270aea62fd0e9e7cba4b4ff9
Cyril Albrecht <albrc1@bfh.ch>
2019-03-08 15:00:12 +0100

    second attempt of image segmentation

--- assignments/planning/segmentation.py
+++ assignments/planning/segmentation.py
@@ -10,130 +10,180 @@ def region_grow(image, seed_point):
     :param seed_point: The seed point for the algorithm
     :return: A 3D binary segmentation mask with the same dimensions as image
     """
-    segmentation_mask = np.zeros(image.shape, np.bool)
-    segmentation_mask[seed_point] = True  # seed point is part of segment
-
-    f = 3  # factor
-    iMax, jMax, kMax = image.shape  # image size
-    i, j, k = seed_point  # seek point coords
-    original_value = image[seed_point]  # value at seed point
 
     # apply threshold
-    tol = 250  # threshold tolerance
-    threshold_mask = threshold2(image, original_value-tol, original_value+tol)
-    print("total number of points in threshold: ", sum(sum(sum(threshold_mask))))
+    f = 5  # number of sigmas, experimental factor
+    values = neighbour_values(image, seed_point)  # values around seed point
+    mu = int(sum(values) / 27)  # mean, 3^3 neighbours including point
+    sigma = int(np.std(values))  # standard deviation
+    print("mu: ", mu, ", sigma: ", sigma)  # debug
+    threshold_mask = threshold2(image, mu-f*sigma, mu+f*sigma)
+    print("total number of points in threshold: ", sum(sum(sum(threshold_mask))))  # debug
 
-    # look for parts belonging to segment, starting from seed point
-    for xx in range(0, iMax):
-        x = max(i - xx, 2)
-        for yy in range(0, jMax):
-            y = max(j - yy, 2)
-            for zz in range(0, kMax):
-                z = max(k - zz, 2)
-                if not threshold_mask[x, y, z]:
-                    continue
-                else:
-                    point = [x, y, z]
-                    segmentation_mask = check_neighbours(image, point, segmentation_mask, f)
+    # make queue of points around seed point
+    queue = enqueue_candidates(threshold_mask, seed_point)
+    print("queue size: ", queue.qsize())  # debug
 
-                z = min(k + zz, kMax-2)
-                if not threshold_mask[x, y, z]:
-                    continue
-                else:
-                    point = [x, y, z]
-                    segmentation_mask = check_neighbours(image, point, segmentation_mask, f)
-
-            y = min(j + yy, jMax-2)
-            for zz in range(0, kMax):
-                z = max(k - zz, 2)
-                if not threshold_mask[x, y, z]:
-                    continue
-                else:
-                    point = [x, y, z]
-                    segmentation_mask = check_neighbours(image, point, segmentation_mask, f)
-
-                z = min(k + zz, kMax-2)
-                if not threshold_mask[x, y, z]:
-                    continue
-                    point = [x, y, z]
-                    segmentation_mask = check_neighbours(image, point, segmentation_mask, f)
-
-        x = min(i + xx, iMax-2)
-        for yy in range(0, jMax):
-            y = max(j - yy, 2)
-            for zz in range(0, kMax):
-                z = max(k - zz, 2)
-                if not threshold_mask[x, y, z]:
-                    continue
-                else:
-                    point = [x, y, z]
-                    segmentation_mask = check_neighbours(image, point, segmentation_mask, f)
-
-                z = min(k + zz, kMax-2)
-                if not threshold_mask[x, y, z]:
-                    continue
-                else:
-                    point = [x, y, z]
-                    segmentation_mask = check_neighbours(image, point, segmentation_mask, f)
-
-            y = min(j + yy, jMax-2)
-            for zz in range(0, kMax):
-                z = max(k - zz, 2)
-                if not threshold_mask[x, y, z]:
-                    continue
-                else:
-                    point = [x, y, z]
-                    segmentation_mask = check_neighbours(image, point, segmentation_mask, f)
-
-                z = min(k + zz, kMax-2)
-                if not threshold_mask[x, y, z]:
-                    continue
-                else:
-                    point = [x, y, z]
-                    segmentation_mask = check_neighbours(image, point, segmentation_mask, f)
-
-    print("total number of points in segmentation: ", sum(sum(sum(segmentation_mask))))
+    # segment with connected threshold algorithm
+    segmentation_mask = segment(queue, threshold_mask, seed_point)
+    print("total number of points in segmentation: ", sum(sum(sum(segmentation_mask))))  # debug
 
     return segmentation_mask
 
 
 def threshold2(image, min, max):
+    """
+    Apply threshold on image with minima and maxima
+    :param image: Image to which to apply the threshold
+    :param min: Minimal value of the threshold
+    :param max: Maximal value of the threshold
+    :return: Threshold mask
+    """
+    threshold_mask = np.zeros(image.shape, np.bool)  # empty mask
+    xMax, yMax, zMax = image.shape  # limits of image
 
-    threshold_mask = np.zeros(image.shape, np.bool)
-    iMax, jMax, kMax = image.shape
-
-    for i in range(0, iMax):
-        for j in range(0, jMax):
-            for k in range(0, kMax):
-                if (image[i, j, k] > min) & (image[i, j, k] < max):
-                    threshold_mask[i, j, k] = True
+    for x in range(0, xMax):
+        for y in range(0, yMax):
+            for z in range(0, zMax):
+                if (image[x, y, z] > min) & (image[x, y, z] < max):
+                    threshold_mask[x, y, z] = True
 
     return threshold_mask
 
 
-def check_neighbours(image, point, mask, f):
+def enqueue_candidates(threshold, point):
+    """
+    Make a queue of all candidates of the segmentation by growing a cube around the point
+    :param threshold: Threshold mask of the image
+    :param point: Starting point of the grow
+    :return: Queue of all candidates
+    """
+    xMax, yMax, zMax = threshold.shape  # shape of the mask
+    xx, yy, zz = point  # initial point coords
+    lim = max(xMax, yMax, zMax)  # limitation of growing
+    q = queue.Queue()  # new empty queue
 
-    x, y, z = point
-    neighbours = sum(sum(sum(mask[x - 1:x + 2, y - 1:y + 2, z - 1:z + 2])))
-    values = neighbour_values(image, point)
-    mu = sum(values) / 27  # 3^3 neighbours including point
-    sigma = np.std(values)
-    if (image[x, y, z] < mu + f * sigma) & (image[x, y, z] > mu - f * sigma) & (neighbours > 0):
-        mask[x, y, z] = True
+    q.put(point)  # add initial point
+
+    # check every surface by growing a cube around initial point
+    for i in range(1, lim):
+        x = min(xx+i, xMax-2)
+        for y in range(yy-i, yy+i):
+            if (y < 0) | (y >= yMax):
+                continue
+            for z in range(zz-i, zz+i):
+                if (z < 0) | (z >= zMax):
+                    continue
+                p = (x, y, z)
+                if threshold[p]:
+                    q.put(p)
+
+        x = max(xx-i, 1)
+        for y in range(yy-i, yy+i):
+            if (y < 0) | (y >= yMax):
+                continue
+            for z in range(zz-i, zz+i):
+                if (z < 0) | (z >= zMax):
+                    continue
+                p = (x, y, z)
+                if threshold[p]:
+                    q.put(p)
+
+        y = min(yy+i, yMax-2)
+        for x in range(xx-i, xx+i):
+            if (x < 0) | (x >= xMax):
+                continue
+            for z in range(zz-i, zz+i):
+                if (z < 0) | (z >= zMax):
+                    continue
+                p = (x, y, z)
+                if threshold[p]:
+                    q.put(p)
+
+        y = max(yy-i, 1)
+        for x in range(xx-i, xx+i):
+            if (x < 0) | (x >= xMax):
+                continue
+            for z in range(zz-i, zz+i):
+                if (z < 0) | (z >= zMax):
+                    continue
+                p = (x, y, z)
+                if threshold[p]:
+                    q.put(p)
+
+        z = min(zz+i, zMax-2)
+        for x in range(xx-i, xx+i):
+            if (x < 0) | (x >= xMax):
+                continue
+            for y in range(yy-i, yy+i):
+                if (y < 0) | (y >= yMax):
+                    continue
+                p = (x, y, z)
+                if threshold[p]:
+                    q.put(p)
+
+        z = max(zz-i, 1)
+        for x in range(xx-i, xx+i):
+            if (x < 0) | (x >= xMax):
+                continue
+            for y in range(yy-i, yy+i):
+                if (y < 0) | (y >= yMax):
+                    continue
+                p = (x, y, z)
+                if threshold[p]:
+                    q.put(p)
+
+    return q
+
+
+def segment(queue, threshold, seed_point):
+    """
+    Segment all points of the queue
+    :param queue: Queue of all candidates of the segmentation
+    :param threshold: Threshold of the image
+    :param seed_point: Seed point of the segmentation
+    :return: Segmentation mask
+    """
+    mask = np.zeros(threshold.shape, np.bool)  # empty mask
+    mask[seed_point] = True  # seed point is part of segment
+
+    # loop through queue and check if neighbours are present
+    while not queue.empty():
+        p = queue.get()
+        mask[p] = check_neighbours(p, mask)
 
     return mask
 
 
-def neighbour_values(image, point):
+def check_neighbours(point, mask):
+    """
+    Check if there are any neighbours around the given point
+    :param point: point to check it's neighbourhood
+    :param mask: Segmentation mask with existing points
+    :return: True if there is at least one neighbour around the point, false if none
+    """
+    x, y, z = point  # coords of point
+    neighbours = sum(sum(sum(mask[x - 1:x + 2, y - 1:y + 2, z - 1:z + 2])))  # total number of neighbours around point
+    if neighbours > 0:
+        return True
+    else:
+        return False
 
-    i, j, k = point
+
+def neighbour_values(image, point):
+    """
+    Get all values around a given point, including itself
+    :param image: The original image where to get the data from
+    :param point: The center point of a 3x3 cube
+    :return: all values as a list
+    """
+    i, j, k = point  # coords of point
     # print("i: ", i, ", j: ", j, ", k: ", k)
 
-    values = []
+    values = []  # empty list of values
     for x in range(i - 1, i + 2):
         for y in range(j - 1, j + 2):
             for z in range(k - 1, k + 2):
                 values.append(image[x, y, z])
-                # print("v: ", image[x, y, z], ", x: ", x, ", y: ", y, ", z: ", z)
 
     return values
